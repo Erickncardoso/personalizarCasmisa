f9c113104469af10e20db512925e701e433aa685
Erick Cardoso <135283010+Erickncardoso@users.noreply.github.com>
2025-05-05 20:51:00 -0300

    add

---
+++ script.js
@@ -0,0 +1,14 @@
+const nome = document.querySelector('#nome')
+const numero = document.querySelector('#numero')
+const btn = document.querySelector('#btn')
+const camisaNome = document.querySelector('#camisa_nome')
+const camisaNumero = document.querySelector('#camisa_numero')
+
+btn.addEventListener('click', ()=>{
+    nomeUser = nome.value
+    camisaNome.innerHTML = nomeUser
+
+
+    numUser = numero.value
+    camisaNumero.innerHTML = numUser
+})
